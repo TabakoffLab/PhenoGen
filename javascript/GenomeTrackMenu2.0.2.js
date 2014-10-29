67ed03ebf9745cb57374522dc45e66addf7491e8
smahaffey <Spencer.Mahaffey@ucdenver.edu>
2014-10-29 16:09:00 -0600

    Cleaned up most of the *Menu.js code with == vs === code Fixed an issue with chrome being detected as both chrome and safari Found and fixed some issues with report conditionals. Moved the call to GenomeSVG into a setTimeout so execution should not be on main thread.

--- javascript/GenomeTrackMenu2.0.2.js
+++ javascript/GenomeTrackMenu2.0.2.js
@@ -27,16 +27,16 @@ function TrackMenu(level){
 		var btData=[];
 		var count=0;
 		for(var j=0;j<that.trackList.length;j++){
-			if(	filter=="all" ||
-				(filter=="allpublic" && that.trackList[j].UserID==0) ||
-				(filter=="custom" && that.trackList[j].UserID!=0) ||
-				(filter=="genome" && that.trackList[j].GenericCategory=="Genome") ||
-				(filter=="trxome" && that.trackList[j].GenericCategory=="Transcriptome")
+			if(	filter==="all" ||
+				(filter==="allpublic" && that.trackList[j].UserID===0) ||
+				(filter==="custom" && that.trackList[j].UserID!==0) ||
+				(filter==="genome" && that.trackList[j].GenericCategory==="Genome") ||
+				(filter==="trxome" && that.trackList[j].GenericCategory==="Transcriptome")
 				){
-				if(  that.trackList[j].Organism.toUpperCase()=="AA" || 
-					 (that.trackList[j].Organism.toUpperCase()==organism.toUpperCase())
+				if(  that.trackList[j].Organism.toUpperCase()==="AA" || 
+					 (that.trackList[j].Organism.toUpperCase()===organism.toUpperCase())
 					){
-					if($("table#trackListTbl"+that.level+" tr#trk"+that.trackList[j].TrackID).length==0){
+					if($("table#trackListTbl"+that.level+" tr#trk"+that.trackList[j].TrackID).length===0){
 						btData[count]=that.trackList[j];
 						count++;
 					}
@@ -51,9 +51,9 @@ function TrackMenu(level){
 			 Bugsense.notify( error, { datatables: "not initialized" } );
 		}
 
-		if(filter=="custom"&&btData.length==0){
+		if(filter==="custom"&&btData.length===0){
 			$("#noCustomTracks").show();
-			if(uid==0){
+			if(uid===0){
 				$("#notSignedIn1").show();
 			}else{
 				$("#notSignedIn1").hide();
@@ -67,32 +67,32 @@ function TrackMenu(level){
 		var tracktbl=d3.select("table#trkSelList"+that.level).select("tbody").selectAll('tr').data(btData)
 				.enter().append("tr")
 				.attr("id",function(d){return "trk"+d.TrackID;})
-				.attr("class",function(d,i){var ret="odd";if(i%2==0){ret="even"} return ret;});
+				.attr("class",function(d,i){var ret="odd";if(i%2===0){ret="even";} return ret;});
 		tracktbl.each(function(d,i){
 				var addtl="";
 				var local="";
-				if(d.Source=="local"){
+				if(d.Source==="local"){
 					local=" (Local)";
 				}
-				if(d.UserID!=0){
+				if(d.UserID!==0){
 					var setup=new String(d.SetupDate);
 					setup=setup.substr(0,setup.indexOf(":",setup.indexOf(":")+1));
 					addtl="<BR><BR>File:"+d.OriginalFile+"<BR><BR>Setup Date:"+setup+"<BR><BR>Type:"+d.Type;
-					if(d.Source=="local"){
+					if(d.Source==="local"){
 						addtl=addtl+"<BR><BR>Local Track only available on this computer.";
 					}
 				}
 				var info="  <span class=\"trlisttooltip"+that.level+"\" title= \""+d.Description+addtl+"\"><img src=\""+iconPath+"info.gif\"></span>";
 
 				d3.select(this).append("td").html(d.Name+local+info);
-				var org=""
-				if(d.Organism=="RN"){
+				var org="";
+				if(d.Organism==="RN"){
 					org="Rat";
-				}else if(d.Organism=="MM"){
+				}else if(d.Organism==="MM"){
 					org="Mouse";
 				}
 				var avail="Public";
-				if(d.UserID!=0){
+				if(d.UserID!==0){
 					avail="Custom";
 				}
 				d3.select(this).append("td").html(org);
@@ -119,47 +119,47 @@ function TrackMenu(level){
 		$("td#selectedTrack"+that.level).hide();
 
 		$('table#trkSelList'+that.level+' tbody').on( 'click', 'tr', function () {
-	        if (! $(this).hasClass('selected') ) {
-	            $('table#trkSelList'+that.level+' tbody tr.selected').removeClass('selected');
-	            $(this).addClass('selected');
-	            var id=$(this).attr("id");
-	            var d=that.findSelectedTrack();
-	            var data="Selected Track Name: "+d.Name+"  <span class=\"trInfotooltip"+that.level+"\" title= \""+d.Description+"\"><img src=\""+iconPath+"info.gif\"></span>";
-	            $("div#trackHeaderOuter"+that.level+" #trackHeaderContent").html(data);
-	            var tblHeight=that.getTrackTableHeight(btData);
-	            //trackDataTable.settings().sScrollY =  tblHeight;
-	            trackDataTable.settings()[0].oScroll.sy=tblHeight;
-	            try{
-					trackDataTable.columns.adjust().draw();
-				}catch(error){
-			 		Bugsense.notify( error, { datatables: "error adjusting columns:132" } );
-				}
-				$('#trkSelList'+that.level+'_wrapper div.dataTables_scroll div.dataTables_scrollBody').css('height', tblHeight);
-	            $("td#selectedTrack"+that.level).show();
-	            $(".trInfotooltip"+that.level).tooltipster({
-						position: 'top-right',
-						maxWidth: 250,
-						offsetX: 10,
-						offsetY: 5,
-						contentAsHTML:true,
-						interactive: true,
-						interactiveTolerance: 350
-					});
-	            //that.generateSettingsDiv(d);
-	           	that.generatePreview(d);
-	           	var track=that.previewSVG.getTrack(d.TrackClass);
-	           	if(track!=undefined && track.generateSettingsDiv!=undefined){
-					track.generateSettingsDiv("td#selectedTrack"+that.level);
-				}
-				$("td#selectedTrack"+that.level+" #trackListTbl"+that.level+" tbody tr:first").remove();
-				$("td#selectedTrack"+that.level+" #trackListTbl"+that.level+" tbody tr:last").remove();
-				if(d.UserID!=0){
-					$("span#deleteCustomTrack"+that.level).show();
-				}else{
-					$("span#deleteCustomTrack"+that.level).hide();
-				}
-	        }
-    	} );
+                        if (! $(this).hasClass('selected') ) {
+                            $('table#trkSelList'+that.level+' tbody tr.selected').removeClass('selected');
+                            $(this).addClass('selected');
+                            var id=$(this).attr("id");
+                            var d=that.findSelectedTrack();
+                            var data="Selected Track Name: "+d.Name+"  <span class=\"trInfotooltip"+that.level+"\" title= \""+d.Description+"\"><img src=\""+iconPath+"info.gif\"></span>";
+                            $("div#trackHeaderOuter"+that.level+" #trackHeaderContent").html(data);
+                            var tblHeight=that.getTrackTableHeight(btData);
+                            //trackDataTable.settings().sScrollY =  tblHeight;
+                            trackDataTable.settings()[0].oScroll.sy=tblHeight;
+                            try{
+                                    trackDataTable.columns.adjust().draw();
+                            }catch(error){
+                                    Bugsense.notify( error, { datatables: "error adjusting columns:132" } );
+                            }
+                            $('#trkSelList'+that.level+'_wrapper div.dataTables_scroll div.dataTables_scrollBody').css('height', tblHeight);
+                            $("td#selectedTrack"+that.level).show();
+                            $(".trInfotooltip"+that.level).tooltipster({
+                                                        position: 'top-right',
+                                                        maxWidth: 250,
+                                                        offsetX: 10,
+                                                        offsetY: 5,
+                                                        contentAsHTML:true,
+                                                        interactive: true,
+                                                        interactiveTolerance: 350
+                                                });
+                            //that.generateSettingsDiv(d);
+                                that.generatePreview(d);
+                                var track=that.previewSVG.getTrack(d.TrackClass);
+                                if(track && track.generateSettingsDiv){
+                                                track.generateSettingsDiv("td#selectedTrack"+that.level);
+                                        }
+                                        $("td#selectedTrack"+that.level+" #trackListTbl"+that.level+" tbody tr:first").remove();
+                                        $("td#selectedTrack"+that.level+" #trackListTbl"+that.level+" tbody tr:last").remove();
+                                        if(d.UserID!==0){
+                                                $("span#deleteCustomTrack"+that.level).show();
+                                        }else{
+                                                $("span#deleteCustomTrack"+that.level).hide();
+                                        }
+                        }
+                } );
 
 		//$('table#trkSelList'+that.level).dataTable().draw();					
 		$(".trlisttooltip"+that.level).tooltipster({
@@ -175,7 +175,7 @@ function TrackMenu(level){
 
 	that.getInitTrackTableHeight=function (btData){
 		var ret="750px";
-		if(btData.length==0){
+		if(btData.length===0){
 			ret="30px";
 		}else if(btData.length<15){
 			ret=(30*btData.length)+"px";
@@ -185,7 +185,7 @@ function TrackMenu(level){
 
 	that.getTrackTableHeight=function (btData){
 		var ret="300px";
-		if(btData.length==0){
+		if(btData.length===0){
 			ret="20px";
 		}else if(btData.length<10){
 			ret=(30*btData.length)+"px";
@@ -195,7 +195,7 @@ function TrackMenu(level){
 
 	that.getTrackData=function(){
 		var tmpContext=contextPath +"/"+ pathPrefix;
-		if(pathPrefix==""){
+		if(pathPrefix===""){
 			tmpContext="";
 		}
 		d3.json(tmpContext+"getBrowserTracks.jsp",function (error,d){
@@ -203,7 +203,7 @@ function TrackMenu(level){
 				console.log(error);
 			}else{
 				that.trackList=d;
-				if(that.level==0){
+				if(that.level===0){
 					trackInfo=[];
 					for(var i=0;i<that.trackList.length;i++){
 						trackInfo[that.trackList[i].TrackClass]=that.trackList[i];
@@ -214,7 +214,7 @@ function TrackMenu(level){
 				//}
 				that.dataInitialized=1;
 				that.generateTrackTable();
-				if(trackMenu[that.level]!=undefined && trackMenu[that.level].readCookieViews!=undefined){
+				if(trackMenu[that.level] && trackMenu[that.level].readCookieViews){
 					trackMenu[that.level].readCookieViews();
 					trackMenu[that.level].generateViewList();
 				}
@@ -226,15 +226,15 @@ function TrackMenu(level){
 		var trackString="";
 		if(isLocalStorage() === true){
 			var cur=localStorage.getItem("phenogenCustomTracks");
-			if(cur!=undefined){
+			if(cur){
 				trackString=cur;
 			}
 		}else{
-			if($.cookie("phenogenCustomTracks")!=null){
+			if($.cookie("phenogenCustomTracks")){
 				trackString=$.cookie("phenogenCustomTracks");
 			}
 		}
-		if(trackString!=null && trackString.indexOf("<;>")>-1){
+		if(trackString && trackString.indexOf("<;>")>-1){
 			var trackStrings=trackString.split("<;>");
 			for(var j=0;j<trackStrings.length;j++){
 				var params=trackStrings[j].split("<->");
@@ -252,7 +252,7 @@ function TrackMenu(level){
 				obj.Source="local";
 				if(params.length>3){
 					that.trackList.push(obj);
-					if(that.level==0){
+					if(that.level===0){
 						trackInfo[obj.TrackClass]=obj;
 					}
 				}
@@ -263,10 +263,10 @@ function TrackMenu(level){
 
 	that.findSelectedTrack=function (){
 		var id=$('table#trkSelList'+that.level+' tbody tr.selected').attr("id");
-		id=new String(id).substr(3);
+		id=id.substr(3);
 		var d=NaN;
 		for(var i=0;i<that.trackList.length&&isNaN(d);i++){
-			if(that.trackList[i].TrackID==id){
+			if(that.trackList[i].TrackID===id){
 				d=that.trackList[i];
 			}
 		}
@@ -282,13 +282,12 @@ function TrackMenu(level){
 	that.generatePreview=function(d){
 		$("div#trackPreviewOuter"+that.level+" div#trackPreviewContent").html("");
 		var tmpOrg=new String(organism).toUpperCase();
-		if(d.Organism=="AA"||d.Organism==tmpOrg){
+		if(d.Organism==="AA"||d.Organism===tmpOrg){
 			var min=svgList[that.level].xScale.domain()[0];
 			var max=svgList[that.level].xScale.domain()[1];
 			that.previewSVG=toolTipSVG("div#trackPreviewOuter"+that.level+" div#trackPreviewContent",565,min,max,that.previewLevel,chr,svgList[that.level].type);
 			that.previewSVG.folderName=svgList[that.level].folderName;
 			var trackString=that.generateSettingsString(d);
-			console.log("Preview Track String:"+trackString);
 			loadStateFromString(trackString,"",that.previewLevel,that.previewSVG);
 			that.previewSVG.updateData();
 			that.previewSVG.updateFullData();
@@ -319,26 +318,26 @@ function TrackMenu(level){
 
 	that.deleteTrack=function(){
 		var d=that.findSelectedTrack();
-		if(d.Source=="local"){
+		if(d.Source==="local"){
 			//remove from the cookie
 			var toDelete="TrackClass="+d.TrackClass+"<->";
 			var trackString="";
 			if(isLocalStorage() === true){
 				var cur=localStorage.getItem("phenogenCustomTracks");
-				if(cur!=undefined){
+				if(cur){
 					trackString=cur;
 				}
 			}else{
-				if($.cookie("phenogenCustomTracks")!=null){
+				if($.cookie("phenogenCustomTracks")){
 					trackString=$.cookie("phenogenCustomTracks");
 				}
 			}
-			if(trackString!=null && trackString.indexOf("<;>")>-1){
+			if(trackString && trackString.indexOf("<;>")>-1){
 				var newTrackString="";
 				var trackStrings=trackString.split("<;>");
 				for(var j=0;j<trackStrings.length;j++){
 					//console.log(trackStrings[j]);
-					if(trackStrings[j].indexOf(toDelete)==-1){
+					if(trackStrings[j].indexOf(toDelete)===-1){
 						newTrackString=newTrackString+trackStrings[j]+"<;>";
 					}
 				}
@@ -360,7 +359,7 @@ function TrackMenu(level){
 	    			async:   false
 				});
 			}
-		}else if(d.Source=="db"){
+		}else if(d.Source==="db"){
 			//remove from the database
 			$.ajax({
 				url:  contextPath +"/"+ pathPrefix +"deleteTrack.jsp",
@@ -368,8 +367,8 @@ function TrackMenu(level){
 				data: {trackID:d.TrackID},
 				dataType: 'json',
     			success: function(data2){
-    				console.log("add track status:"+data2.success);
-    				if(data2.success=="true"){
+    				//console.log("add track status:"+data2.success);
+    				if(data2.success==="true"){
     					$(".uploadStatus").html("<B>Track Setup Successfully</B>");
     				}else{
     					$(".uploadStatus").html("<B>Track Setup Failed</B>");
@@ -407,9 +406,9 @@ function TrackMenu(level){
 		$("div#confirmUpload"+that.level).hide();
 		customTrackLevel=that.level;
 		var type=$("#usrtrkFileTypeSelect"+that.level).val();
-		if(type=="bed"||type=="bg"){
+		if(type==="bed"||type==="bg"){
 			that.createUploadedCustomTrack();
-		}else if(type=="bb"||type=="bw"){
+		}else if(type==="bb"||type==="bw"){
 			that.createRemoteCustomTrack();
 		}
 	};
@@ -427,24 +426,24 @@ function TrackMenu(level){
 			while(fTrunc.indexOf(".")>-1){
 				fTrunc=fTrunc.substr(fTrunc.indexOf(".")+1);
 			}
-			if(fTrunc!="" && fTrunc!=fName){
+			if(fTrunc!=="" && fTrunc!==fName){
 				fExt=fTrunc;
 			}
 		}
 		//check file size and extension
 		if(fSize<20){
-			if(fExt!=type){
-				if(fExt=="gz"||fExt=="tar"||fExt=="zip"||fExt=="exe"||fExt=="bin"){
+			if(fExt!==type){
+				if(fExt==="gz"||fExt==="tar"||fExt==="zip"||fExt==="exe"||fExt==="bin"){
 					//cancel with no support
 					setTimeout(function(){
 	        				$("div#uploadBtn"+customTrackLevel).show();
 	        				$(".progressInd").hide();
 	        			},5000);
 					$(".uploadStatus").html("Error: Selected File Type is not supported.");
-				}else if($("input#hasconfirmBed"+that.level).val()==0){
+				}else if($("input#hasconfirmBed"+that.level).val()===0){
 					//display confirmation
 					$("div#confirmBed"+customTrackLevel).show();
-				}else if($("input#hasconfirmBed"+that.level).val()==1){
+				}else if($("input#hasconfirmBed"+that.level).val()===1){
 					$("div#confirmBed"+customTrackLevel).hide();
 					//continue
 					that.createTrack(file);
@@ -469,7 +468,7 @@ function TrackMenu(level){
 		var file=$("#customFileURL"+that.level).val();
 		var session="";
 		var tmp=new Date();
-		if($.cookie("JSESSIONID")!=null){
+		if($.cookie("JSESSIONID")){
     		session=$.cookie("JSESSIONID");
     		session=session+"_"+tmp.getTime();
     	}
@@ -477,7 +476,7 @@ function TrackMenu(level){
 	};
 
 	that.saveTrack=function(trackClass,file){
-		console.log("saving track uid:"+uid);
+		//console.log("saving track uid:"+uid);
 		//if uid==0 not logged in >0 should save back to DB.
 		var trackName=$("#usrtrkNameTxt"+that.level).val();
 		var desc=$("#usrtrkDescTxt"+that.level).val();
@@ -488,14 +487,14 @@ function TrackMenu(level){
 		var loc=file;
 		var type=$("#usrtrkFileTypeSelect"+that.level).val();
 		var prevFile="";
-		if(type=="bed"||type=="bg"){
+		if(type==="bed"||type==="bg"){
 			prevFile=$("input#customUploadFile"+that.level)[0].files[0].name;
 		}
 		var colorSel="Color";
 		if($("#usrtrkColorSelect"+that.level).length>0){
 			colorSel=$("#usrtrkColorSelect"+that.level).val();
 		}
-		if(colorSel=="Score"){
+		if(colorSel==="Score"){
 			var minCol="000000";
 			var maxCol="FFFFFF";
 			var minVal=1;
@@ -515,26 +514,26 @@ function TrackMenu(level){
 			maxVal=$("#urstrkScoreMaxTxt"+that.level).val();
 			
 		}
-		if(type=="bed"){
+		if(type==="bed"){
 			control="Name=View Density;Select:trackSelect:Dense:Select;{Dense:1}{Pack:3}{Full:2};Default=3,Name=Color by;Select:colorSelect::colorSelect;{Feature Defined:Color}{Score based scale:Score};Default="+colorSel+",Name=Scale Range;Txt:tissuecbx;{min:1}{max:1000};Default="+minVal+":"+maxVal+",Name=Color Range;Txt:color;{min:FFFFFF}{max:000000};Default="+minCol+":"+maxCol;
-		}else if(type=="bg"){
+		}else if(type==="bg"){
 			control="Name=View Density;Select:trackSelect:Dense:Select;{Dense:1}{Full:2};Default=1,Name=Scale Range;Slider:rangeslider;{min:1-1000}{max:1000-20000};Default=1:5000";
-		}else if(type=="bw"){
+		}else if(type==="bw"){
 			control="Name=View Density;Select:trackSelect:Dense:Select;{Dense:1}{Full:2};Default=1,Name=Scale Range;Slider:rangeslider;{min:1-1000}{max:1000-20000};Default=1:5000";
-		}else if(type=="bb"){
+		}else if(type==="bb"){
 			control="Name=View Density;Select:trackSelect:Dense:Select;{Dense:1}{Pack:3}{Full:2};Default=3,Name=Color by;Select:colorSelect::colorSelect;{Feature Defined:Color}{Score based scale:Score};Default="+colorSel+",Name=Scale Range;Txt:tissuecbx;{min:1}{max:1000};Default="+minVal+":"+maxVal+",Name=Color Range;Txt:color;{min:FFFFFF}{max:000000};Default="+minCol+":"+maxCol;
 		}
 		trackClass="custom"+trackClass;
 		if(uid>0){
-			console.log("saving track to db");
+			//console.log("saving track to db");
 			$.ajax({
 				url:  contextPath +"/"+ pathPrefix +"addTrack.jsp",
    				type: 'GET',
 				data: {trackClass: trackClass,trackName: trackName, trackDesc:desc,trackOrg:org,genericCategory:genCat,category:cat,controls:control,location:loc,type:type,file:prevFile},
 				dataType: 'json',
     			success: function(data2){
-    				console.log("add track status:"+data2.success);
-    				if(data2.success=="true"){
+    				//console.log("add track status:"+data2.success);
+    				if(data2.success==="true"){
     					$(".uploadStatus").html("<B>Track Setup Successfully</B>");
     				}else{
     					$(".uploadStatus").html("<B>Track Setup Failed</B>");
@@ -556,12 +555,12 @@ function TrackMenu(level){
 			customTrackStr=customTrackStr+"<;>";
 			if(isLocalStorage() === true){
 				var cur=localStorage.getItem("phenogenCustomTracks");
-				if(cur!=undefined){
+				if(cur){
 					customTrackStr=cur+customTrackStr;
 				}
 				localStorage.setItem("phenogenCustomTracks",customTrackStr);
 			}else{
-				if($.cookie("phenogenCustomTracks")!=null){
+				if($.cookie("phenogenCustomTracks")){
 					customTrackStr=$.cookie("phenogenCustomTracks")+customTrackStr;
 				}
 				$.cookie("phenogenCustomTracks",customTrackStr);
@@ -622,7 +621,7 @@ function TrackMenu(level){
 	        			$("div#uploadBtn"+customTrackLevel).show();
 	    			}
 				});
-	}
+	};
 
 	that.progressHandlingFunction=function(e){
 		$(".progressInd").show();
