726697b8a9c561a53ef515fcde5d035bd218b6f8
smahaffey <Spencer.Mahaffey@ucdenver.edu>
2015-03-17 11:40:45 -0600

    Updated extractor to extract transcript upstream sequences updated script embeded in meme results jsp file.

--- perl/scripts/readEnsemblUpstreamSeq.pl
+++ perl/scripts/readEnsemblUpstreamSeq.pl
@@ -13,7 +13,7 @@ sub extractUpstream
         open IN, "<",$input;
         while(<IN>){
             my $line=$_;
-            print $line;
+            #print $line;
             $line =~ s/^\s+//;
             $line =~ s/\s+$//;
             push (@genelist,$line);
@@ -84,14 +84,15 @@ sub extractUpstream
 	foreach my $gene(@genelist){
             if(length($gene)>0){
                 my $seq="";
-                my $name="";
+                
                 my @list=split(";",$gene);
-                print $gene."\n";
-                my$ensG=$geneAdaptor->fetch_by_stable_id($list[0]);
+                #print $gene."\n";
+                my $ensG=$geneAdaptor->fetch_by_stable_id($list[0]);
                 if($type eq 'gene'){
+                    my $name="";
                     $name=$list[0];
                     if(@list>1){
-                        $name=$name." | ".$list[1];
+                        $name=$name." | GeneListIDs ".$list[1];
                     }
                     my $start=$ensG->seq_region_start()-$len;
                     my $stop=$ensG->seq_region_start();
@@ -99,18 +100,57 @@ sub extractUpstream
                         $start=$ensG->seq_region_end();
                         $stop=$ensG->seq_region_end()+$len;
                     }
-                    print "start:".$ensG->seq_region_start()."\tstop:".$ensG->seq_region_end()."\t strand:".$ensG->strand()."\n";
-                    print "seq=".$start."-".$stop."\t".$ensG->seq_region_name()."\n";
+                    if($start<1){
+                        $start=1;
+                    }
+                    #print "start:".$ensG->seq_region_start()."\tstop:".$ensG->seq_region_end()."\t strand:".$ensG->strand()."\n";
+                    #print "seq=".$start."-".$stop."\t".$ensG->seq_region_name()."\n";
                     my $ensGSlice=$slice_adaptor->fetch_by_region('chromosome', $ensG->seq_region_name(), $start, $stop ,$ensG->strand());
                     $seq=${$seq_adaptor->fetch_by_Slice_start_end_strand($ensGSlice)};
-                    print "Seq:".$seq."\n";
+                    #print "Seq:".$seq."\n";
+                    if(not($seq eq "")){
+                        print OUT ">".$name."\n";
+                        print OUT $seq."\n";
+                    }
                 }else{
+                    my $transcripts=$ensG->get_all_Transcripts ();
+                    #print "gene start:".$ensG->seq_region_start()."\tstop:".$ensG->seq_region_end()."\t strand:".$ensG->strand()."\n";
+                    $cntTranscripts = 0;
+		    while ( my $transcript = shift @{$transcripts} ) {
+                        my $initStart=$transcript->seq_region_start();
+                        my $initStop=$transcript->seq_region_end();
+                       
+                        if($type eq "trxcds"){
+                            $initStart=$transcript->coding_region_start();
+                            $initStop=$transcript->coding_region_end();
+                        }
+                        #print "coding:".$initStart."-".$initStop."\n";
+                        my $start=$initStart-$len;
+                        my $stop=$initStart;
+                        if($ensG->strand() == -1){
+                            $start=$initStop;
+                            $stop=$initStop+$len;
+                        }
+                        if($start<1){
+                            $start=1;
+                        }
+                        #print $transcript->stable_id()."\n".$ensG->seq_region_name().":".$start."-".$stop."\n";
+                        my $ensTSlice=$slice_adaptor->fetch_by_region('chromosome', $ensG->seq_region_name(), $start, $stop ,$ensG->strand());
+                        $seq="";
+                        $seq=${$seq_adaptor->fetch_by_Slice_start_end_strand($ensTSlice)};
+                        if(not($seq eq "")){
+                            my $name=$list[0]." | ".$transcript->stable_id();
+                            if(@list>1){
+                                $name=$name." | GeneListIDs ".$list[1];
+                            }
+                            print OUT ">".$name."\n";
+                            print OUT $seq."\n";
+                        }
+                    }
 
+                    
                 }
-                if(not($seq eq "")){
-                    print OUT ">".$name."\n";
-                    print OUT $seq."\n";
-                }
+                
             }
         }	
 
