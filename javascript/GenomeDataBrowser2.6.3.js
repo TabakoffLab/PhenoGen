454ae19761a36f2907130b621b9b8124c0c75fe7
Spencer Mahaffey <Spencer.Mahaffey@ucdenver.edu>
2017-05-31 13:53:23 -0600

    increment browser js version

--- javascript/GenomeDataBrowser2.6.3.js
+++ javascript/GenomeDataBrowser2.6.3.js
@@ -86,7 +86,7 @@ mouseOnly.probeMouse=1;
 
 var mmVer="Mouse(<span id=\"verSelect\"></span>) Strain:C57BL/6J";
 var rnVer="Rat(<span id=\"verSelect\"></span>) Strain:BN";
-var siteVer="PhenoGen v3.3.1(4/30/2017)";
+var siteVer="PhenoGen v3.3.2(4/30/2017)";
 
 var trackBinCutoff=10000;
 var customTrackLevel=-1;
