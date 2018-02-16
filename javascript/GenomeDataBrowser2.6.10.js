552220adf4fc48470e6aa443894f27a59166dbd7
Spencer Mahaffey <Spencer.Mahaffey@ucdenver.edu>
2018-02-16 15:03:04 -0700

    Update Dates, annotation info, release details.

--- javascript/GenomeDataBrowser2.6.10.js
+++ javascript/GenomeDataBrowser2.6.10.js
@@ -86,7 +86,7 @@ mouseOnly.probeMouse=1;
 
 var mmVer="Mouse(<span id=\"verSelect\"></span>) Strain:C57BL/6J";
 var rnVer="Rat(<span id=\"verSelect\"></span>) Strain:BN";
-var siteVer="PhenoGen v3.4.1(2/20/2018)";
+var siteVer="PhenoGen v3.4.1(2/16/2018)";
 
 var trackBinCutoff=10000;
 var customTrackLevel=-1;
